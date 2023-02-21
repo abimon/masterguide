990a8d3dec71333ee3778d1f6d6f12a651c8ee69
abimon <eddyabby59@gmail.com>
2023-02-21 23:01:54 +0300

    remove loader spinner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,78 +31,7 @@
 
 	<!-- Modernizer for Portfolio -->
 	<script src="{{asset('storage/js/modernizer.js')}}"></script>
-	<style>
-		#loader {
-			transition: all 0.3s ease-in-out;
-			opacity: 1;
-			visibility: visible;
-			position: fixed;
-			height: 100vh;
-			width: 100%;
-			background: #fff;
-			z-index: 90000;
-		}
-
-		#loader.fadeOut {
-			opacity: 0;
-			visibility: hidden;
-		}
-
-		.spinner {
-			width: 40px;
-			height: 40px;
-			position: absolute;
-			top: calc(50% - 20px);
-			left: calc(50% - 20px);
-			background-color: #333;
-			border-radius: 100%;
-			-webkit-animation: sk-scaleout 0.5s infinite ease-in-out;
-			animation: sk-scaleout 0.5s infinite ease-in-out;
-		}
-
-		@-webkit-keyframes sk-scaleout {
-			0% {
-				-webkit-transform: scale(0)
-			}
-
-			100% {
-				-webkit-transform: scale(1.0);
-				opacity: 0;
-			}
-		}
-
-		@keyframes sk-scaleout {
-			0% {
-				-webkit-transform: scale(0);
-				transform: scale(0);
-			}
-
-			100% {
-				-webkit-transform: scale(1.0);
-				transform: scale(1.0);
-				opacity: 0;
-			}
-		}
-
-		a {
-			text-decoration: none;
-		}
-
-		a :hover {
-			color: #03F4FC;
-		}
-	</style>
-	<div id='loader'>
-		<div class="spinner"></div>
-	</div>
-	<script>
-		window.addEventListener('load', function load() {
-			const loader = document.getElementById('loader');
-			setTimeout(function() {
-				loader.classList.add('fadeOut');
-			}, 10);
-		});
-	</script>
+	
 </head>
 
 <body class="bg-info fs-sm-5">
