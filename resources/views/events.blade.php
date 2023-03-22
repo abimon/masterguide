9086f61b680980f4c5f067d6b54b3ad03e631d7d
abimon <eddyabby59@gmail.com>
2023-03-22 10:30:49 +0300

    descrption styling

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -18,8 +18,8 @@
                             <span class="price-value">Kshs {{$event->charges}}.00<span class="month"> only</span> <span>{{$event->event_date}} </span><span>{{$event->venue}}</span></span>
                         </div>
 
-                        <div class="pricingContent" style="height: 150px; overflow:hidden;">
-                            <div style="font-size: small;"><?php echo html_entity_decode($event['event_description']);?></div>
+                        <div class="pricingContent p-2" style="height: 150px; overflow:hidden;">
+                            <div><?php echo html_entity_decode($event['event_description']);?></div>
                         </div><!-- /  CONTENT BOX-->
                        
                         <div class="pricingTable-sign-up">
