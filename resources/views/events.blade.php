4512e4d3ba53bd602e48bbe9e18c427ee73c4485
abimon <eddyabby59@gmail.com>
2023-03-22 10:29:05 +0300

    event description

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -19,7 +19,7 @@
                         </div>
 
                         <div class="pricingContent" style="height: 150px; overflow:hidden;">
-                            <div style="font-size: small;"><?php html_entity_decode($event['event_description']);?></div>
+                            <div style="font-size: small;"><?php echo html_entity_decode($event['event_description']);?></div>
                         </div><!-- /  CONTENT BOX-->
                        
                         <div class="pricingTable-sign-up">
