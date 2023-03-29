b0707e3378ce59ee494218fbe918f2c8a2d7facb
abimon <eddyabby59@gmail.com>
2023-03-29 09:49:48 +0300

    mpesa number sms

--- app/Http/Controllers/dataController.php
+++ app/Http/Controllers/dataController.php
@@ -354,7 +354,7 @@ class dataController extends Controller
     function bulk(){
         $users=activity::all();
         foreach($users as $user){
-            $message='Hello '.($user->attendee).'! Thank you for registering to attend the Master Guide Joint Sabbath. We humbly request that you send KSHs 150 to facilitate your entrance and lunch on that day before Thursday 30th March 2023 12:00pm. Thank you. Regards University Region.';
+            $message='Hello '.($user->attendee).'! Thank you for registering to attend the Master Guide Joint Sabbath. We humbly request that you send KSHs 150 to +254714298174 to facilitate your entrance and lunch on that day before Thursday 30th March 2023 12:00pm. Thank you. Regards University Region.';
             $this->sms($message, $user->contact);
         }
         
