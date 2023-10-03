7660dff132547d6589a3e85177a1ae1b7fe56d58
abimon <eddyabby59@gmail.com>
2023-10-03 09:42:10 +0300

    show reg

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,3 +1,4 @@
+@section('dashboard')
 <link rel="stylesheet" href="{{asset('storage/css/bootstrap.min.css')}}">
 <link rel="stylesheet" href="{{asset('storage/css/adminator.css')}}">
 <div style="text-align: center;">
@@ -19,22 +20,52 @@
                         </div>
                     </div>
                 </div>
-                <div style="margin:0; display:inline-block">
-                    @foreach($users as $key=>$user)
-                    <div style="width:30%; margin:5px; float:left;">
-                        <div style="align-content: center;">
-                            <div style="align-content: center;">
-                                <img src="{{asset('storage/profile/'.$user->avatar)}}" class="card-img-top" style="width:100px;height:150px;" >
+                <!-- #Sales Report ==================== -->
+                <div class="bd bgc-white">
+                    <div class="layers" id="members">
+                        <div class="layer w-100">
+                            <div class="bgc-light-blue-500 c-white p-20">
+                                <div class="peers ai-c jc-sb gap-40">
+                                    <div class="peer peer-greed">
+                                        <h5>{{date('F j, Y')}}</h5>
+                                        <p class="mB-0">Event Attendance</p>
+                                    </div>
+                                    <div class="peer">
+                                        <h3 class="text-right text-light">{{$users->count()}}</h3>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="card-body text-center text-light">
-                                <h5 class="card-title">{{$user->name}}</h5>
-                                <p class="card-text">{{$user->institution}}</p>
+                            <div class="table-responsive p-20">
+                                <form action="" method="post">
+                                    @csrf
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th class=" bdwT-0">#</th>
+                                                <th class=" bdwT-0">Name</th>
+                                                <th class=" bdwT-0">Institution</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach($users as $key=>$user)
+                                            <tr>
+                                                <th>{{$key+1}}</th>
+                                                <td>{{$user->name}}</td>
+                                                <td>{{$user->institution}}</td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                    <div class="modal-footer">
+                                        <button type="submit" formaction="/print/{{$title}}" class="btn btn-outline-info">Print</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
-                    @endforeach
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
