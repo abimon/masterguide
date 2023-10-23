6369e6c7d207ec6731467e14269cf72faebe542c
Abimon <eddyabby59@gmail.com>
2023-10-23 16:29:43 +0300

    chat

--- app/Http/Controllers/API/chatController.php
+++ app/Http/Controllers/API/chatController.php
@@ -14,7 +14,7 @@ class chatController extends Controller
         $users = User::all();
         $messages = [];
         foreach ($users as $user) {
-            $message = Conversation::join('users','users.id',$user->id)->where([['recepient_id', '=', $id], ['sender_id', '=', $user->id], ['isPrivate', '=', 1]])->orWhere([['sender_id', '=', $id], ['recepient_id', '=', $user->id], ['isPrivate', '=', 1]])->latest()->take(1)->first();
+            $message = Conversation::join('users','users.id',$user->id)->where([['recepient_id', '=', $id], ['sender_id', '=', $user->id]])->orWhere([['sender_id', '=', $id], ['recepient_id', '=', $user->id]])->latest()->take(1)->first();
             if ($message) {
                 array_push($messages, $message);
             }
