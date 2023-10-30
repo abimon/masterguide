5bcd2fe402c3614100dbb0087164879ce910af39
Abimon <eddyabby59@gmail.com>
2023-10-30 21:33:59 +0300

    blog articles

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -8,16 +8,10 @@
 </div>
     <div id="overviews" class="section wb">
         <div class="container">
-
-            <hr class="invis"> 
-
             <div class="row"> 
 				@foreach($posts as $post)
                 <div class="col-lg-4 col-md-6 col-12">
                     <div class="blog-item">
-						<div class="image-blog">
-							<img src="{{asset('storage/images/blog_1.jpg')}}" alt="" class="img-fluid">
-						</div>
 						<div class="meta-info-blog">
 							<span><i class="fa fa-calendar"></i> <a href="#">{{date_format(($post->created_at), 'F j, Y')}}</a> </span>
                             <span><i class="fa fa-thumbs-up"></i>  <a href="#">{{$likes->where('post_id',$post->id)->count()}}</a> </span>
@@ -27,7 +21,7 @@
 							<h2><a href="#" title="">{{$post->title}}</a></h2>
 						</div>
 						<div class="blog-desc">
-						<div style="overflow:hidden;height: 100px;" ><?php echo html_entity_decode($post['post']);?></div>...
+						<div style="overflow:hidden;height: 109px;"><?php echo html_entity_decode($post['post']);?></div>...
 						</div>
 						<div class="blog-button">
 							<a class="hover-btn-new orange" href="/blogpost/{{$post->title}}"><span>Read More<span></a>
