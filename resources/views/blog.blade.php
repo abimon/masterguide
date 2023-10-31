78fc0e5dee1f2170ab3d730377dd7d17a59f4705
Abimon <eddyabby59@gmail.com>
2023-10-31 13:41:45 +0300

    edit

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,31 +6,33 @@
 		<h1>Articles of Read</h1>
 	</div>
 </div>
-    <div id="overviews" class="section wb">
-        <div class="container">
-            <div class="row"> 
-				@foreach($posts as $post)
-                <div class="col-lg-4 col-md-6 col-12">
-                    <div class="blog-item" >
-						<div class="meta-info-blog">
-							<span><i class="fa fa-calendar"></i> <a href="#">{{date_format(($post->created_at), 'F j, Y')}}</a> </span>
-                            <span><i class="fa fa-thumbs-up"></i>  <a href="#">{{$likes->where('post_id',$post->id)->count()}}</a> </span>
-                            <span><i class="fa fa-comments"></i> <a href="#">{{$comments->where('post_id',$post->id)->count()}} Comments</a></span>
-						</div>
+<div id="overviews" class="section wb">
+	<div class="container">
+		<div class="row">
+			@foreach($posts as $post)
+			<div class="col-lg-4 col-md-6 col-12">
+				<div class="blog-item">
+					<div class="meta-info-blog">
+						<span><i class="fa fa-calendar"></i> <a href="#">{{date_format(($post->created_at), 'F j, Y')}}</a> </span>
+						<span><i class="fa fa-thumbs-up"></i> <a href="#">{{$likes->where('post_id',$post->id)->count()}}</a> </span>
+						<span><i class="fa fa-comments"></i> <a href="#">{{$comments->where('post_id',$post->id)->count()}} Comments</a></span>
+					</div>
+					<div style="height: max-content;">
 						<div class="blog-title">
-							<h2><a href="#" title="">{{$post->title}}</a></h2>
+							<h2 class="text-transform:uppercase;">{{$post->title}}</h2>
 						</div>
 						<div class="blog-desc">
-						<div style="overflow:ellipsis;height: 100px;">{{$post->biography}}</div>
-						</div>
-						<div class="blog-button">
-							<a class="hover-btn-new orange" href="/blogpost/{{$post->title}}"><span>Read More<span></a>
+							<div style="overflow:ellipsis;max-height: 100px;">{{$post->bio}}</div>
 						</div>
 					</div>
-                </div><!-- end col -->
-				@endforeach
-            </div><!-- end row -->
-            <hr class="hr3"> 
-        </div><!-- end container -->
-    </div><!-- end section -->
+					<div class="blog-button">
+						<a class="hover-btn-new orange" href="/blogpost/{{$post->title}}"><span>Read More<span></a>
+					</div>
+				</div>
+			</div><!-- end col -->
+			@endforeach
+		</div><!-- end row -->
+		<hr class="hr3">
+	</div><!-- end container -->
+</div><!-- end section -->
 @endsection
