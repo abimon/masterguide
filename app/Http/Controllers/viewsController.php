b074a37ed3ba5efd720f84f4a27d7cbdfeecb46f
Abimon <eddyabby59@gmail.com>
2023-11-09 15:12:54 +0300

    events and lessons

--- app/Http/Controllers/viewsController.php
+++ app/Http/Controllers/viewsController.php
@@ -117,6 +117,7 @@ class viewsController extends Controller
         ];
         return view('events',$data);
     }
+    
     function attendance(){
         if((Auth()->user()->role=='Coordinator')||(Auth()->user()->role=='Secretary')||(Auth()->user()->role=='Training Coordinator')){
             $users=User::all();
@@ -172,5 +173,9 @@ class viewsController extends Controller
             'users'=>$users
         ];
         return view('attendee',$data);
+    }
+    function compose(){
+        $data=[];
+        return view('compose',$data);
     }
 }
